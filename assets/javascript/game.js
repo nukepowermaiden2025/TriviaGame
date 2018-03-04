4d7ed534258475417dc6533ee9b62f7d5f159116
Kourtney Reynolds <Kourtney.Reynolds@gmail.com>
2018-03-03 23:16:04 -0700

    Selectors added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,36 @@
-//Global Variables
-    var riAns;
-    var wrAns;
-    var timer;
+ //Global Variables
+ var riAns = 0;
+ var wrAns = 0;
+ var timer;
+
+var trivia = {
+   
+   
+//Set the selectors to change
+    //Right answer div generation
+    rightS: $("<p>").text("You guessed"+ riAns + "right." ),
+
+    //Wrong answer div generation
+    wrongS: $("<p>").text("You guessed"+ wrAns + "wrong." ),
+
+    //Timer
+
+    //Question
+    quesDisS:$("ques-display").text($(this).QnAobj.ques),
+
+    //Answer Choice
+    
+
+    //Correct Ans
+    corAnsS:$("corAns-display").text($(this).QnAobj.corAns),
+
+    //Image shows when right answer is selected
+
+    imgKeyS:$("imgkey-display").html($(this).QnAobj.imgkey),
+
+    //Append data to page
+
+
 
 //Make a question object
     //key1 = question
@@ -9,22 +38,22 @@
     //key3 = correct answer
     //key4 = image
 
-var trivia = {
-
-    quesAns:[{
+    QnAobj:[{
         ques:"First RandomWord Question 1",
         ansArr:["ans1", "ans2", "ans3", "ans4"],
         corAns:"ans1",
-        imgkey:"<img"
-    
-    
-    
-    },
-        
-
-
+        imgkey:"<img src='./assets/images/test.jpg' />"
+        }
     ]
     
+
+
+
+
+
+
+
+
 
 }
 
@@ -36,20 +65,6 @@ var trivia = {
     //Process questions asked
     //Resets Timer
     //Reset Game
-
-
-//Set the selectors to change
-    //Right answer div generation
-    //Wrong answer div generation
-    //Timer
-    //Question
-    //Answer Choice
-    //Correct Ans
-
-
-
-
-
 
 //Set the timeer & display first Question
 
