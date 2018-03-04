4c993067cce276356843e94a05f2a5bac05e881a
Kourtney Reynolds <Kourtney.Reynolds@gmail.com>
2018-03-03 22:08:21 -0700

    test object

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,71 @@
+//Global Variables
+    var riAns;
+    var wrAns;
+    var timer;
+
 //Make a question object
     //key1 = question
-    //key2 = answer array options
+    //key2 = answer array optionsgit
     //key3 = correct answer
     //key4 = image
 
-//Set the timeer & Bring out first Question
+var trivia = {
+
+    quesAns:[{
+        ques:"First RandomWord Question 1",
+        ansArr:["ans1", "ans2", "ans3", "ans4"],
+        corAns:"ans1",
+        imgkey:"<img"
+    
+    
+    
+    },
+        
+
+
+    ]
+    
+
+}
+
+
+
+//Functions
+    //Randomly chooses questions
+    //Process player guess
+    //Process questions asked
+    //Resets Timer
+    //Reset Game
+
+
+//Set the selectors to change
+    //Right answer div generation
+    //Wrong answer div generation
+    //Timer
+    //Question
+    //Answer Choice
+    //Correct Ans
+
+
+
+
+
+
+//Set the timeer & display first Question
 
 //Display the oject . question 
 //Display the object . answer array
 //Count Down Timer
 
 //If player guesses correctly
+    //Call Timer Reset
+    //Increment Right
+//Else player guess wrong
+    //Call Timer Reset
+    //Increment Wrong Answer
 
+
+
+
+
+   
