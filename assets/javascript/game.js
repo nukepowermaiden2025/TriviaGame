0fbb357bf6a4f95bde73290c51fbd4530133f0f2
Kourtney Reynolds <Kourtney.Reynolds@gmail.com>
2018-03-03 19:13:58 -0700

    Orignial Files creation

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+//Make a question object
+    //key1 = question
+    //key2 = answer array options
+    //key3 = correct answer
+    //key4 = image
+
+//Set the timeer & Bring out first Question
+
+//Display the oject . question 
+//Display the object . answer array
+//Count Down Timer
+
+//If player guesses correctly
+
