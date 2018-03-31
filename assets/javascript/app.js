a7cd8c97ffe6240806676fc7faebbaba41d8250e
Kourtney Reynolds <Kourtney.Reynolds@gmail.com>
2018-03-30 21:03:42 -0700

    added Reset game, and counter reset, and correct answer display

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,7 +3,7 @@ var countStartNumber = 30;
 
 // Question for the player
 var questions = [{
-  question: "What is the capitol city of Ohio?",
+  question: "What is the capitol city of China?",
   answers: ["Cincinatti", "Cleveland", "Dayton", "Columbus"],
   correctAnswer: "Columbus",
   image: ""
@@ -34,12 +34,45 @@ var timer;//variable will control the set interval method
 var trivia = {
 
     //Put out the initals variable for the game
-    //Load a question
-        //Load another question after that
-    //Count down the time
-    //Action to happend when the time is up
-    //What to show if choice is correct
-    //What ot show if choice is incorrect
+
+    questions: questions,
+    currentQuestion: 0,
+    counter: countStartNumber,
+    correct: 0,
+    incorrect: 0,
+
+    //Reset
+    reset: function() {
+        this.currentQuestion = 0;
+        this.counter = countStartNumber;
+        this.correct = 0;
+        this.incorrect = 0;
+        this.loadQuestion();
+      },
+
+    //Count down the time 
+    countdown: function(){
+        trivia.counter--;
+        $("#display-time").text(trivia.counter);
+        if(trivia.counter ===0){
+            console.log("Your Time ran out")
+            clearInterval(timer);
+
+        //reset counter
+        $("#counter-number").html(trivia.counter);
+        // and if the time is up show the answer
+        panel.html("<h2>Out of Time!</h2>");
+        panel.append("<h3>The Correct Answer was: " + questions[this.currentQuestion].correctAnswer);
+        panel.append("<img src='" + questions[this.currentQuestion].image + "' />");
+
+        if (trivia.currentQuestion === questions.length - 1) {
+        setTimeout(trivia.results, 3 * 1000);
+        }
+        else {
+        setTimeout(trivia.nextQuestion, 3 * 1000);
+        }
+            }
+        }
     //Show the results
     //reset the game
 
